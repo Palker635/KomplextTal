9b2158eaaedbd8e9337f1caeb98e72fd75af95eb
isac.hansson <Isac.hansson@elev.ga.ntig.se>
2020-02-07 11:13:23 +0100

    sedrftgyhuj

--- src/komplex.java
+++ src/komplex.java
@@ -5,42 +5,43 @@ public class komplex {
     public komplex(double a, double b) {
         this.a = a;
         this.b = b;
+    }
 
-
+    public komplex(komplex add) {
     }
 
     public static void main(String[] args) {
         komplex a = new komplex(3, 2);
         komplex b = new komplex(4, 1);
-        komplex c = a.add(b);
-        komplex d = a.multiply(b);
+
+        komplex c = add(a, b);
+        komplex d = multiply(a, b);
+
 
         System.out.println(c);
+        if (c.equals(d)) {
+            System.out.println("Add funkar");
+        }
+
+        c = multiply(a, b);
+        d = new komplex(10,11);
+
+        System.out.println(c);
+        if (c.equals(d)) {
+            System.out.println("Multiply funkar");
+        }
+
     }
 
     private static komplex add(komplex a, komplex b) {
-            komplex c = new komplex();
-        return c;
+        komplex c = new komplex(add(a, b));
+        System.out.println(c);
+        return null;
     }
 
     private static komplex multiply(komplex a, komplex b) {
-            komplex d = new komplex();
-        return d;
+        komplex d = new komplex(multiply(a, b));
+        System.out.println(d);
+        return null;
     }
-
-
-
-
-
-
-
-
-
-
-    public String toString() {
-        return Re + " + " + Im + "i";
-
-    }
-    private double getIm
-
 }
